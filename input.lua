eca3e30bdc61935533ddfe373838a66df1d82f0a
enzdor <enzo01lb@gmail.com>
2023-11-05 14:05:46 +0100

    can draw cards from stock only when stock is clicked

--- input.lua
+++ input.lua
@@ -24,15 +24,19 @@ love.mousereleased = function(x, y, button)
 	y = mouse_pos.getRealY(y)
 	if button == 1 then
 		-- draw card from stock to waste
-		for i = #entities.entities.stock.cards, 1, -1 do
-			entities.entities.stock.cards[i].x_pos = entities.entities.waste.x_pos
-			entities.entities.stock.cards[i].y_pos = entities.entities.waste.y_pos
-			entities.entities.stock.cards[i].x_pos_orig = entities.entities.waste.x_pos
-			entities.entities.stock.cards[i].y_pos_orig = entities.entities.waste.y_pos
-			entities.entities.stock.cards[i].face_up = true
-			entities.entities.waste.cards[#entities.entities.waste.cards+1] = entities.entities.stock.cards[i]
-			table.remove(entities.entities.stock.cards, i)
-			return
+		if x > entities.entities.stock.x_pos and x < entities.entities.stock.x_pos + state.card.width
+		    and y > entities.entities.stock.y_pos and y < entities.entities.stock.y_pos + state.card.height then
+			for i = #entities.entities.stock.cards, 1, -1 do
+				entities.entities.stock.cards[i].x_pos = entities.entities.waste.x_pos
+				entities.entities.stock.cards[i].y_pos = entities.entities.waste.y_pos
+				entities.entities.stock.cards[i].x_pos_orig = entities.entities.waste.x_pos
+				entities.entities.stock.cards[i].y_pos_orig = entities.entities.waste.y_pos
+				entities.entities.stock.cards[i].face_up = true
+				entities.entities.waste.cards[#entities.entities.waste.cards + 1] = entities.entities
+				    .stock.cards[i]
+				table.remove(entities.entities.stock.cards, i)
+				return
+			end
 		end
 		-- drag card around
 		-- for _, entity in ipairs(entities.entities) do
